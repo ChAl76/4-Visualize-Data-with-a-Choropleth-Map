1228f3ead99cdba61b6551805ea5566fe5ab5010
ChAl76 <chornyalexander@gmail.com>
2024-12-17 18:05:07 +0200

    define tooltip & color scale

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ const svg = d3
   .attr('id', 'map')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom);
+
+// Tooltip
+const tooltip = d3
+  .select('body')
+  .append('div')
+  .attr('id', 'tooltip')
+  .style('opacity', 0);
+
+// Define color scale
+const colorScale = d3
+  .scaleThreshold()
+  .domain([10, 20, 30, 40, 50, 60])
+  .range(['#f0f9e8', '#bae4bc', '#7bccc4', '#43a2ca', '#0868ac', '#023457']);
